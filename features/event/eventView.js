9f205bb610144925389560a8d351cb8c9d21df78
r77chen <ryanchen706@gmail.com>
2024-06-21 15:41:19 -0700

    Update eventView.js

--- features/event/eventView.js
+++ features/event/eventView.js
@@ -68,7 +68,7 @@ class EventView {
     actionsTd.classList.add("event-actions");
 
     const saveBtn = document.createElement("button");
-    saveBtn.classList.add("save-btn");
+    saveBtn.classList.add("add-btn");
     saveBtn.innerHTML = `
       <svg focusable viewBox="0 0 24 24" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
         <path d="M12 6V18M18 12H6" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
